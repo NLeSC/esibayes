87b18b6d8fd752384d5b543e2cdc1a46ee3f2521
Jurriaan H. Spaaks <jspaaks@uva.nl>
2012-12-19 15:15:06 +0100

    edited the doc for sodaAbortOptim and sodaAllComb and find/replaced scempar with conf in the code

--- esibayes/mmsoda/sodaAbortOptim.m
+++ esibayes/mmsoda/sodaAbortOptim.m
@@ -1,18 +1,18 @@
-function converged = sodaAbortOptim(critGelRub,scemPar)
+function converged = sodaAbortOptim(critGelRub,conf)
 %
-% <a href="matlab:web(fullfile(scemroot,'html','abortoptim.html'),'-helpbrowser')">View HTML documentation for this function in the help browser</a>    
+% <a href="matlab:web(fullfile(scemroot,'html','sodaAbortOptim.html'),'-helpbrowser')">View HTML documentation for this function in the help browser</a>    
 %
 
 
-if size(critGelRub,1)>=scemPar.nGelRub
+if size(critGelRub,1)>=conf.nGelRub
     
-    A = critGelRub(end-scemPar.nGelRub+1:end,scemPar.parCols);
+    A = critGelRub(end-conf.nGelRub+1:end,conf.parCols);
         
     D = max(A,[],1) - min(A,[],1);
     
     
-    if all(D < scemPar.convMaxDiff) &&...
-       all(critGelRub(end,scemPar.parCols)<scemPar.critGelRubConvd)
+    if all(D < conf.convMaxDiff) &&...
+       all(critGelRub(end,conf.parCols)<conf.critGelRubConvd)
         disp('Convergence achieved.')
         converged = true;
     else
