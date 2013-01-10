82c8450af02f8125e3c6ad4ca6829decb367e929
Jurriaan H. Spaaks <jspaaks@uva.nl>
2013-01-10 16:58:21 +0100

    new constants method

---
+++ esibayes/bypass-so-double-normal/makeconstants.m
@@ -0,0 +1,11 @@
+function makeconstants()
+
+parMu1 = -10;
+parSigma1 = 3;
+parMu2 = 5;
+parSigma2 = 1;
+
+save('./data/constants.mat')
+
+
+
--- esibayes/bypass-so-double-normal/model/calcLikelihood.m
+++ esibayes/bypass-so-double-normal/model/calcLikelihood.m
@@ -1,16 +1,11 @@
-function objScore = calcLikelihood(mConstants,allStateValuesKF,allValuesNOKF,parVec)
+function objScore = calcLikelihood(conf,constants,allStateValuesKF,allValuesNOKF,parVec)
 % function doublenormal calculates the density directly, so calculating the
 % likelihood is a matter of passing the information from allValuesNOKF on to
 % the variable objScore;
 
 
-% map the parameter values to their respective variables:
-x = parVec(1);
+sodaUnpack()
 
-% map the constants values to their respective variables:
-for iConstant=1:size(mConstants,1)
-    eval([mConstants{iConstant,1},'=mConstants{iConstant,2};'])
-end
 
 dens = (1/(sqrt(2*pi*parSigma1^2))*exp(-(1/2)*((x-parMu1)/parSigma1)^2) + ...
         1/(sqrt(2*pi*parSigma2^2))*exp(-(1/2)*((x-parMu2)/parSigma2)^2))/2;
