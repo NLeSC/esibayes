ed7ebc21ebc41bd6d15987fd697ccbf62f32967d
Jurriaan H. Spaaks <jspaaks@uva.nl>
2013-01-28 17:52:05 +0100

    minor change

--- esibayes/mmsoda/tools/sodaroot.m
+++ esibayes/mmsoda/tools/sodaroot.m
@@ -1,7 +1,7 @@
 function s=sodaroot
-% <a href="matlab:web(fullfile(newscem_root,'html','license.html'),'-helpbrowser')">show license statement</a> 
+% <a href="matlab:web(fullfile(sodaroot,'html','license.html'),'-helpbrowser')">show license statement</a> 
 %
-a=mfilename('fullpath');
-Ix=findstr(a,filesep);
-s=a(1:Ix(end-1));
+a = mfilename('fullpath');
+Ix = findstr(a,filesep);
+s = a(1:Ix(end-1));
 
