351e9c78556e3f5d6d9a445d9e6f0403195bbf8a
Jurriaan H. Spaaks <jspaaks@uva.nl>
2014-01-23 16:08:36 +0100

    the results can now be archived after every generation

--- src/mmsoda-toolbox/mmsoda.m
+++ src/mmsoda-toolbox/mmsoda.m
@@ -429,7 +429,6 @@ elseif isempty(varargin)
             end
 
 
-
             if mod(iGeneration,conf.saveInterval)==0
 
                 save(['./results/',conf.modeStr,soMoStr,'-tempstate.mat'],'-mat',...
@@ -437,6 +436,13 @@ elseif isempty(varargin)
 
             end %  mod(iGeneration,...
 
+            if conf.executeInParallel
+                if conf.archiveResults
+                    tmp = textscan(getenv('PBS_JOBID'),'%[^.]');
+                    jobidStr = tmp{1}{1};
+                    copyfile('results',['results',jobidStr]);
+                end
+            end
 
         end % mmsodaContinue(conf,nModelEvals)
 
